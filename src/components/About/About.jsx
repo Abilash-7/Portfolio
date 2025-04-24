53523c0e54be5893af8e00dd70cfe50bd8d1b377
Abilash-7 <abilashaa8127416@gmail.com>
2025-04-24 13:37:52 +0530

    about chnage

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
     {/* about */}
         <motion.div variants={textvariants} className="text-container text-center py-lg-5 pb-md-5 p-md-0">
             <h1 className='display-5 fw-bold  pb-3 title' >ABOUT</h1>
-            <p className='px-5'>  
+            <p className='px-lg-5  desc'>  
                 Hello! My name is Abilash, and I have completed my studies in B.Tech (Information Technology) in 2023. I developed an interest in web development during my college years. I experimented with basic HTML programs, including forms, buttons, and especially CSS, which added a more attractive look and feel to the HTML. 
                 I thoroughly enjoyed the output that appeared in the web browser; 
                 it truly impressed me.
@@ -46,7 +46,7 @@ const About = () => {
         </motion.div>
 
         {/* resume */}
-        <motion.div variants={textvariants} className="resume  text-center py-md-5  py-lg-0 py-sm-5">    
+        <motion.div variants={textvariants} className="resume text-center py-md-5  py-lg-0 py-sm-5">    
             <h1 className='fw-bold pb-sm-3'>Resume</h1>
             <div className="buttons d-flex justify-content-center gap-5 p-3">
                 <a href="../../../public/assets/Abilash M_Resume.pdf" download className='btn text-black linkbtn'><i class="fa-solid fa-download pe-3 "></i>Get Resume</a>
